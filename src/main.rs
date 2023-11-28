4b011b3c6f3330f8de2bf7eb83c443e62e7dfbaf
Mike Upton <me@uptonm.dev>
2023-11-28 18:41:13 -0500

    cch day -1

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/-1/error")]
+fn error() -> &'static str {
+    panic!()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, error]);
 
     Ok(rocket.into())
 }
